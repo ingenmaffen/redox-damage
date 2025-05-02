2e9c692711077e92a6dfcbef871280c4d2fe6eea
Róbert Rabi <ingenmaffen@gmail.com>
2025-05-02 14:57:48 +0200

    Fix overflow issues for inc and dec instructions

--- src/emu/instructions/decrement.rs
+++ src/emu/instructions/decrement.rs
@@ -5,11 +5,15 @@ use super::enums::InstructionSourceTarget;
 
 pub fn dec(cpu: &mut CPU, target: InstructionSourceTarget) {
     match target {
-        InstructionSourceTarget::BC => cpu.registers.set_bc(cpu.registers.get_bc() - 1),
-        InstructionSourceTarget::DE => cpu.registers.set_de(cpu.registers.get_de() - 1),
-        InstructionSourceTarget::HL => cpu.registers.set_hl(cpu.registers.get_hl() - 1),
-        InstructionSourceTarget::SP => cpu.sp -= 1,
+        InstructionSourceTarget::BC => cpu.registers.set_bc(get_new_value_after_dec_u16(cpu.registers.get_bc())),
+        InstructionSourceTarget::DE => cpu.registers.set_de(get_new_value_after_dec_u16(cpu.registers.get_de())),
+        InstructionSourceTarget::HL => cpu.registers.set_hl(get_new_value_after_dec_u16(cpu.registers.get_hl())),
+        InstructionSourceTarget::SP => cpu.sp = get_new_value_after_dec_u16(cpu.sp),
         InstructionSourceTarget::HlAsPointer => panic!("This is handled in a separate function"),
+        InstructionSourceTarget::BcAsPointer => panic!("Target not supported"),
+        InstructionSourceTarget::DeAsPointer => panic!("Target not supported"),
+        InstructionSourceTarget::HlMinus => panic!("Target not supported"),
+        InstructionSourceTarget::HlPlus => panic!("Target not supported"),
         _ => dec_r8(cpu, target),
     }
     cpu.pc += 1;
@@ -17,50 +21,56 @@ pub fn dec(cpu: &mut CPU, target: InstructionSourceTarget) {
 
 pub fn dec_r8_at_hl(cpu: &mut CPU, memory: &mut Memory) {
     let address: usize = cpu.registers.get_hl() as usize;
-    memory.addresses[address] += 1;
-    let new_value = memory.addresses[address];
-    set_dec_flags(new_value, cpu);
+    memory.addresses[address] = get_new_value_after_dec(memory.addresses[address]);
+    set_dec_flags(memory.addresses[address], cpu);
 }
 
 fn dec_r8(cpu: &mut CPU, target: InstructionSourceTarget) {
     match target {
         InstructionSourceTarget::B => {
-            cpu.registers.b -= 1;
-            let new_value = cpu.registers.b;
-            set_dec_flags(new_value, cpu);
+            cpu.registers.b = get_new_value_after_dec(cpu.registers.b);
+            set_dec_flags(cpu.registers.b, cpu);
         }
         InstructionSourceTarget::C => {
-            cpu.registers.c -= 1;
-            let new_value = cpu.registers.c;
-            set_dec_flags(new_value, cpu);
+            cpu.registers.c = get_new_value_after_dec(cpu.registers.c);
+            set_dec_flags(cpu.registers.c, cpu);
         }
         InstructionSourceTarget::D => {
-            cpu.registers.d -= 1;
-            let new_value = cpu.registers.d;
-            set_dec_flags(new_value, cpu);
+            cpu.registers.d = get_new_value_after_dec(cpu.registers.d);
+            set_dec_flags(cpu.registers.d, cpu);
         }
         InstructionSourceTarget::E => {
-            cpu.registers.e -= 1;
-            let new_value = cpu.registers.e;
-            set_dec_flags(new_value, cpu);
+            cpu.registers.e = get_new_value_after_dec(cpu.registers.e);
+            set_dec_flags(cpu.registers.e, cpu);
         }
         InstructionSourceTarget::H => {
-            cpu.registers.h -= 1;
-            let new_value = cpu.registers.h;
-            set_dec_flags(new_value, cpu);
+            cpu.registers.h = get_new_value_after_dec(cpu.registers.h);
+            set_dec_flags(cpu.registers.h, cpu);
         }
         InstructionSourceTarget::L => {
-            cpu.registers.l -= 1;
-            let new_value = cpu.registers.l;
-            set_dec_flags(new_value, cpu);
+            cpu.registers.l = get_new_value_after_dec(cpu.registers.l);
+            set_dec_flags(cpu.registers.l, cpu);
         }
         InstructionSourceTarget::A => {
-            cpu.registers.a -= 1;
-            let new_value = cpu.registers.a;
-            set_dec_flags(new_value, cpu);
+            cpu.registers.a = get_new_value_after_dec(cpu.registers.a);
+            set_dec_flags(cpu.registers.a, cpu);
         }
-        _ => (),
+        _ => panic!("Unhandled case"),
     }
+}
+
+fn get_new_value_after_dec(original_value: u8) -> u8 {
+    if original_value == 0 {
+        return u8::MAX;
+    }
+    return original_value - 1;
+}
+
+fn get_new_value_after_dec_u16(original_value: u16) -> u16 {
+    if original_value == 0 {
+        return u16::MAX;
+    }
+    return original_value - 1;
 }
 
 fn set_dec_flags(new_value: u8, cpu: &mut CPU) {
--- src/emu/instructions/increment.rs
+++ src/emu/instructions/increment.rs
@@ -5,11 +5,15 @@ use super::enums::InstructionSourceTarget;
 
 pub fn inc(cpu: &mut CPU, target: InstructionSourceTarget) {
     match target {
-        InstructionSourceTarget::BC => cpu.registers.set_bc(cpu.registers.get_bc() + 1),
-        InstructionSourceTarget::DE => cpu.registers.set_de(cpu.registers.get_de() + 1),
-        InstructionSourceTarget::HL => cpu.registers.set_hl(cpu.registers.get_hl() + 1),
-        InstructionSourceTarget::SP => cpu.sp += 1,
+        InstructionSourceTarget::BC => cpu.registers.set_bc(get_new_value_after_inc_u16(cpu.registers.get_bc())),
+        InstructionSourceTarget::DE => cpu.registers.set_de(get_new_value_after_inc_u16(cpu.registers.get_de())),
+        InstructionSourceTarget::HL => cpu.registers.set_hl(get_new_value_after_inc_u16(cpu.registers.get_hl())),
+        InstructionSourceTarget::SP => cpu.sp = get_new_value_after_inc_u16(cpu.sp),
         InstructionSourceTarget::HlAsPointer => panic!("This is handled in a separate function"),
+        InstructionSourceTarget::BcAsPointer => panic!("Target not supported"),
+        InstructionSourceTarget::DeAsPointer => panic!("Target not supported"),
+        InstructionSourceTarget::HlMinus => panic!("Target not supported"),
+        InstructionSourceTarget::HlPlus => panic!("Target not supported"),
         _ => inc_r8(cpu, target),
     }
     cpu.pc += 1;
@@ -25,42 +29,49 @@ pub fn inc_r8_at_hl(cpu: &mut CPU, memory: &mut Memory) {
 fn inc_r8(cpu: &mut CPU, target: InstructionSourceTarget) {
     match target {
         InstructionSourceTarget::B => {
-            cpu.registers.b += 1;
-            let new_value = cpu.registers.b;
-            set_inc_flags(new_value, cpu);
+            cpu.registers.b = get_new_value_after_inc(cpu.registers.b);
+            set_inc_flags(cpu.registers.b, cpu);
         }
         InstructionSourceTarget::C => {
-            cpu.registers.c += 1;
-            let new_value = cpu.registers.c;
-            set_inc_flags(new_value, cpu);
+            cpu.registers.c = get_new_value_after_inc(cpu.registers.c);
+            set_inc_flags(cpu.registers.c, cpu);
         }
         InstructionSourceTarget::D => {
-            cpu.registers.d += 1;
-            let new_value = cpu.registers.d;
-            set_inc_flags(new_value, cpu);
+            cpu.registers.d = get_new_value_after_inc(cpu.registers.d);
+            set_inc_flags(cpu.registers.d, cpu);
         }
         InstructionSourceTarget::E => {
-            cpu.registers.e += 1;
-            let new_value = cpu.registers.e;
-            set_inc_flags(new_value, cpu);
+            cpu.registers.e = get_new_value_after_inc(cpu.registers.e);
+            set_inc_flags(cpu.registers.e, cpu);
         }
         InstructionSourceTarget::H => {
-            cpu.registers.h += 1;
-            let new_value = cpu.registers.h;
-            set_inc_flags(new_value, cpu);
+            cpu.registers.h = get_new_value_after_inc(cpu.registers.h);
+            set_inc_flags(cpu.registers.h, cpu);
         }
         InstructionSourceTarget::L => {
-            cpu.registers.l += 1;
-            let new_value = cpu.registers.l;
-            set_inc_flags(new_value, cpu);
+            cpu.registers.l = get_new_value_after_inc(cpu.registers.l);
+            set_inc_flags(cpu.registers.l, cpu);
         }
         InstructionSourceTarget::A => {
-            cpu.registers.a += 1;
-            let new_value = cpu.registers.a;
-            set_inc_flags(new_value, cpu);
+            cpu.registers.a = get_new_value_after_inc(cpu.registers.a);
+            set_inc_flags(cpu.registers.a, cpu);
         }
-        _ => (),
+        _ => panic!("Unhandled case"),
     }
+}
+
+fn get_new_value_after_inc(original_value: u8) -> u8 {
+    if original_value == u8::MAX {
+        return 0;
+    }
+    return original_value + 1;
+}
+
+fn get_new_value_after_inc_u16(original_value: u16) -> u16 {
+    if original_value == u16::MAX {
+        return 0;
+    }
+    return original_value + 1;
 }
 
 fn set_inc_flags(new_value: u8, cpu: &mut CPU) {
