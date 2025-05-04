36e75a8552eceb307bbc8e345780be69631134eb
Rabi Róbert <ingenmaffen@gmail.com>
2025-05-05 00:21:43 +0200

    Add boot sequence

---
+++ src/emu/boot.rs
@@ -0,0 +1,46 @@
+use super::memory::Memory;
+
+pub fn boot_sequence(memory: &mut Memory) {
+    unpack_and_load_logo(memory);
+}
+
+fn unpack_and_load_logo(memory: &mut Memory) {
+    let mut memory_index: usize = 0x8010;
+    for i in 0x0104..=0x0133 {
+        let byte = memory.addresses[i as usize];
+        let upper = (byte & 0b11110000) >> 4;
+        let lower = byte & 0b00001111;
+        load_to_vram(memory, upper, memory_index);
+        memory_index += 4;
+        load_to_vram(memory, lower, memory_index);
+        memory_index += 4;
+    }
+}
+
+fn load_to_vram(memory: &mut Memory, value: u8, index: usize) {
+    let unpacked = get_current_value_unpacked(value);
+    let mut memory_index = index;
+    for _ in 0..2 {
+        memory.addresses[memory_index] = unpacked;
+        memory_index += 1;
+        memory.addresses[memory_index] = 0x00;
+        memory_index += 1;
+    }
+}
+
+fn get_current_value_unpacked(byte: u8) -> u8 {
+    let mut value: u8 = 0x00;
+    if byte & 0b1000 > 0 {
+        value = value | 0b11000000;
+    }
+    if byte & 0b0100 > 0 {
+        value = value | 0b00110000;
+    }
+    if byte & 0b0010 > 0 {
+        value = value | 0b00001100;
+    }
+    if byte & 0b0001 > 0 {
+        value = value | 0b00000011;
+    }
+    return value;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod emu;
 
+use emu::boot;
 use emu::cpu::CPU;
 use emu::instruction_mapper;
 use emu::memory::Memory;
@@ -16,8 +17,10 @@ fn main() {
         memory.load_rom_data_into_bank_01(romdata);
     }
 
+    boot::boot_sequence(&mut memory);
     cpu.pc = 0x0100;
     cpu.sp = 0xFFFE;
+
     while cpu.pc < 0xFFFF {
         instruction_mapper::execute_instruction(&mut cpu, &mut memory);
     }
